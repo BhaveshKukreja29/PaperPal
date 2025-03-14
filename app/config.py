5ba75b9f910cbfce7eae10d1d4fab0bfec8db4ba
BhaveshKukreja29 <bhaveshkukreja29@gmail.com>
2025-03-14 13:39:06 +0530

    added user keys

---
+++ app/config.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+CONFIG_DIR = os.path.expanduser("~/app")
+CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
+
+def getKey():
+    if not os.path.exists(CONFIG_DIR):
+        os.makedirs(CONFIG_DIR)
+
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as f:
+            config = json.load(f)
+
+            if "API_KEY" in config:
+                return config["API_KEY"] 
+    
+    API_KEY = input("Paste your Gemini API key: ").strip()
+    with open(CONFIG_FILE, "w") as f:
+        json.dump({"API_KEY": API_KEY}, f)
+    
+    return API_KEY
--- app/summarizer.py
+++ app/summarizer.py
@@ -1,16 +1,18 @@
 import google.generativeai as genai
-import os
-
-API_KEY = os.getenv("LLM_API_KEY")
+from config import getKey
 
 class Summarizer:
     def __init__(self, text):
         self.summary = self.summarise(text)
 
     def summarise(self, text):
+        API_KEY = getKey()
         genai.configure(api_key=API_KEY)
+        
+        try:
+            model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+            response = model.generate_content(f'Please summarize the following text in a nice manner: {text}')
 
-        model = genai.GenerativeModel(model_name='gemini-1.5-flash')
-        response = model.generate_content(f'Please summarize the following text in a nice manner: {text}')
-
-        return response.text
+            return response.text
+        except:
+            return "Some error occured during summarization :("
